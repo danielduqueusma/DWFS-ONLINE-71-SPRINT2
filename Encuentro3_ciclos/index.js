ca5f50c747fb164cc573dd793fdd5b7aa7e30c37
Daniel Duque <daniel.duque@grey.com>
2021-03-29 22:21:06 -0500

    Encuentro 3

---
+++ Encuentro3_ciclos/index.js
@@ -0,0 +1,64 @@
+
+// let numToEvaluate = 10
+// let name = 'Daniel'
+
+// for (let i = 1; i <= 3; i++) {
+//   console.log('Daniel' + i)
+// }
+
+// switch (name) {
+//   case 'Daniel':
+//     console.log('La variable vale: ' + name)
+//     break
+//   case 'Vera':
+//     console.log('La variable vale: ' + name)
+//     break
+//   case 'Ramiro':
+//     console.log('La variable vale: ' + name)
+//     break
+//   case 'Gabriela':
+//     console.log('La variable vale: ' + name)
+//     break
+//   case 'Ciro':
+//     console.log('La variable vale: ' + name)
+//     break
+//   default:
+//     console.log('Nos fuimos por el default y La variable vale: ' + name)
+//     break
+// }
+
+let name = 'Daniel Duque'
+let n1 = 2
+let n2 = 5
+let n3 = 78
+let cualquierCosa = 'DWFS'
+
+// printNames(name, n1, n2, n3, cualquierCosa)
+// printNames('Nadia', 10, 20, 30, 'Escoba')
+// printNames('Vera', 20, 30, 50, 'Jarra')
+// printNames('Belen', 30, 50, 40, 'Trapero')
+// printNames('Gabriela', 40, 40, 50, 'Trapo')
+// printNames('Mi mamá', 50, 20, 50, 'Jugo')
+
+// function printNames (river, boca, nacional, medellin, cosa) {
+//   let name = 'Usma'
+//   let n1 = 30
+//   console.log(river, boca)
+// }
+
+
+// function mostrarEdades (edad) {
+//   let resultado = edad * 2
+//   return resultado
+// }
+
+// printNames('Mi mamá', 50, 20, 50, 'Jugo')
+// let resultadoMosrarEdadesDaniel = mostrarEdades(29)
+// let resultadoMosrarEdadesOtra = mostrarEdades(25)
+
+// console.log(resultadoMosrarEdadesDaniel, resultadoMosrarEdadesOtra)
+let i = 1
+do {
+  console.log(i)
+  i = i + 1
+} while (i < 0)
